b8d3f41cb6cc4679f17e10c701dfe772c99e08b3
realjcwkm <realjcwkm@gmail.com>
2021-05-22 21:53:33 -0400

    add header component

---
+++ src/components/header/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Nav, Ul, Li } from './styles';
+
+export default function NavBar() {
+  return (
+    <Nav>
+      <Ul>
+        <Li>Premium</Li>
+        <Li>Suporte</Li>
+        <Li>Baixar</Li>
+        <Li>|</Li>
+        <Li>Inscrever-se</Li>
+        <Li>Entrar</Li>
+      </Ul>
+    </Nav>
+  );
+};
---
+++ src/components/header/spanIcon/styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const DivSpanIcon = styled.div`
+  align-items: center;
+`;
+
+export const ASpanIcon = styled.a`
+  text-indent: -9999px;
+  white-space: nowrap;
+  overflow: hidden;
+  color: transparent;
+  width: 136px;
+`;
+
+export const Span = styled.span`
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ImgIcon = styled.img`
+  padding-top: 10px;
+  width: 136px;
+  height: 48px;
+`;
